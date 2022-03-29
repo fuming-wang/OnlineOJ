4a26946ab99cad803b563cf4e5cd18ebb5084973
Minria <810760487@qq.com>
2022-03-29 21:41:04 +0800

    build on 2022/3/29

---
+++ src/main/java/com/example/onlineoj/OnlineOjApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlineoj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineOjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineOjApplication.class, args);
+    }
+
+}
